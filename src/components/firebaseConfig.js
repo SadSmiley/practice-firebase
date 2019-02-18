67737fac5659b5a27eaeb90bb672b3b99e2bfab3
Edward <you@example.com>
2019-02-18 21:26:38 +0800

    Initialize

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyATL2B4b9mzvCuYunseW4KyG2nwWZVuIck",
+  authDomain: "practice-firebase-62908.firebaseapp.com",
+  databaseURL: "https://practice-firebase-62908.firebaseio.com",
+  projectId: "practice-firebase-62908",
+  storageBucket: "practice-firebase-62908.appspot.com",
+  messagingSenderId: "798691936778"
+}
